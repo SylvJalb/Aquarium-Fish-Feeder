c30b01b0b6d6a16b5aee805b1936eddac316bafe
Sylvain JALBERT <sylv.jalbert@gmail.com>
2022-02-17 21:53:24 +0100

    simple log writer et reader with requests

--- feeder.py
+++ feeder.py
@@ -1,4 +1,8 @@
+import datetime
+import os
 from flask import Flask
+from flask import request
+
 from env import *
 
 app = Flask(__name__)
@@ -8,6 +12,31 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
-@app.route('/feed')
+@app.route('/feed', methods=['GET', 'POST'])
 def feed():
-    return 'OK ! I drop some food.'
+    log_file = "./feeder.log"
+    if request.method == 'GET':
+        # Check if the log file exists
+        if os.path.isfile(log_file):
+            # return content of feeder.log
+            with open(log_file, 'r') as f:
+                content = f.read()
+            # replace newline with <br>
+            content = content.replace('\n', '<br>')
+            return content
+        else:
+            return "No log file found."
+    if request.method == 'POST':
+        when = request.form['feed']
+        if when == 'now':
+            # feed now
+            with open(log_file, 'a') as f:
+                # write time without microseconds
+                f.write("\n" + str(datetime.datetime.now(timezone(timezone_name)).replace(microsecond=0)))
+            return 'OK ! I drop some food...'
+    with open(log_file, 'a') as f:
+            f.write("\n"  + str(datetime.datetime.now(timezone(timezone_name)).replace(microsecond=0)) + " Problem with the request")
+    return 'Problem with your request...'
+
+if __name__ == '__main__':
+   app.run(debug = True)
